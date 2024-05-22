73818264d587e17a1121e937188b885932fc64e0
EloraBarua <elorabarua62@gmail.com>
2024-05-22 20:55:40 +0600

    testing for production

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    // origin: ["https://dhruva-parisad-clientside.vercel.app"],
-    origin: ["http://localhost:3000"],
+    origin: ["https://dhruva-parisad-clientside.vercel.app"],
+    // origin: ["http://localhost:3000"],
     credentials: true,
   })
 );
